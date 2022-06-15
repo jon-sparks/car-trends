a32207267cfc971ac2b9bce208a4ed049b0404c0
Jon Sparks <jon.sparks@sotic.net>
2022-06-15 12:45:32 +0100

    mobile snippets

--- components/snippets/Snippets.vue
+++ components/snippets/Snippets.vue
@@ -13,10 +13,18 @@ export default {
 <style lang="scss" scoped>
 .snippets {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   gap: 2rem;
 
-  @media(min-width: 768px) {
+  @media(min-width: 640px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media(min-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media(min-width: 1680px) {
     grid-template-columns: repeat(6, 1fr);
   }
 }
